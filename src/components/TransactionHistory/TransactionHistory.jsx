29e76b9f50e7ea071792cf3d1f7a88befe8e8a1d
Kateryna-Kupriianova <kupriyanov.forever@gmail.com>
2024-07-21 19:58:15 +0300

    add_styles_table

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,13 +1,14 @@
 import TransactionHistoryItem from "../TransactionHistoryItem/TransactionHistoryItem"
+import css from "./TransactionHistory.module.css";
 
 const TransactionHistory = ({ items }) => {
     return (
-        <table>
+        <table className={css.table}>
             <thead>
                 <tr>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Currency</th>
+                    <th className={css.thText}>Type</th>
+                    <th className={css.thText}>Amount</th>
+                    <th className={css.thText}>Currency</th>
                 </tr>
             </thead>
 
--- src/components/TransactionHistoryItem/TransactionHistoryItem.jsx
+++ src/components/TransactionHistoryItem/TransactionHistoryItem.jsx
@@ -1,9 +1,10 @@
+import css from "./TransactionHistoryItem.module.css"
 const TransactionHistoryItem = ({ type, amount, currency }) => {
     return (
         <tr>
-          <td>{type}</td>
-          <td>{amount}</td>
-          <td>{currency}</td>
+          <td className={css.tdType}>{type}</td>
+          <td className={css.tdAmount}>{amount}</td>
+          <td className={css.tdCurrency}>{currency}</td>
         </tr>
     )
 }
